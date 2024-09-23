2b38c8cc67eaecc03146da6101369f95090aab60
Denis Louka <louka0407@gmail.com>
2024-09-23 08:40:15 +0200

    Redirect in loginPage if no token

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -4,22 +4,23 @@ let auth = async (req, res, next) => {
   try {
     let token = req.cookies.w_auth;
 
-    // Trouver l'utilisateur par le token
+    if (!token) {
+      return res.status(401).json({ isAuth: false, message: 'Token non fourni. Redirection vers la page de connexion.' });
+    }
+
     const user = await User.findByToken(token);
 
     if (!user) {
-      return res.json({
-        isAuth: false,
-        error: true
-      });
+      return res.status(401).json({ isAuth: false, message: 'Authentification échouée. Redirection vers la page de connexion.' });
     }
 
     req.token = token;
     req.user = user;
     next();
   } catch (err) {
-    return err;
+    return res.status(401).json({ isAuth: false, message: 'Erreur serveur. Redirection vers la page de connexion.' });
   }
 };
+
 
 module.exports = { auth };
